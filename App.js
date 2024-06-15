47e5cc0497447aaa8343d6b1fd6c46abab19e025
shafeerashafu <shafeerazahur3@gmail.com>
2024-06-15 20:37:03 +0530

    Course 2

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const ele=React.createElement("div",
 {id:"heading",className:"head"},  //Attributes
-"Hello from React");
+"Hello from React Course");
 
 const nestedelement=React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
-    React.createElement("h1",{},"I am nested child")
+    React.createElement("h1",{},"I am nested child from React")
     )
 )
 
---
+++ README.md
@@ -0,0 +1,21 @@
+# React.js Course
+
+# parcel
+
+-dev build (Dev dependency)
+
+-it runs on local server
+
+-HMR -> (Hot Modeule Replacement) ->render
+
+-File Watching Algorithm -> written in c++
+
+-Faster development builds because of caching
+
+-Bundling
+
+-minification
+
+-compression
+
+-Tree Shaking -> remove unused code
